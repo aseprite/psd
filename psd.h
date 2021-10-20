2fe7a5b9e23cc7daaf0fb1112aa1fbad98822035
Joshua Ogunyinka <ogunyinkajoshua@yahoo.com>
2021-10-20 15:34:34 +0400

    Merge Decoder's onImageScanline

--- psd.h
+++ psd.h
@@ -422,18 +422,13 @@ namespace psd {
   };
 
   struct GlobalMaskInfo {
-    enum class Opacity: uint16_t {
-      Transparent = 0,
-      Opaque = 100
-    };
-
     enum class MaskKind: uint8_t {
       Inverted = 0,
       ColorProtected = 1,
       ExactPixelValue = 128,
     };
 
-    Opacity  opacity;
+    uint16_t opacity;
     MaskKind kind;
   };
 
@@ -517,11 +512,6 @@ namespace psd {
                                  const ChannelID chanID,
                                  const uint8_t* data,
                                  const int bytes) { }
-    // Emitted for RAW images
-    virtual void onImageScanline(const ImageData& imgData,
-                                 const int y,
-                                 const ChannelID chanID,
-                                 const std::vector<uint32_t>& data) { }
     virtual void onEndImage(const ImageData& img) { }
   };
 
